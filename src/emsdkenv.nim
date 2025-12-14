e2bdc38d8b091cb009849e3c40b4eca465a83c81
zenywallet <zenywallet@bitzeny.jp>
2025-12-15 01:00:45 +0900

    add emsdkenv source list

--- src/emsdkenv.nim
+++ src/emsdkenv.nim
@@ -6,8 +6,10 @@ export emsdkenv
 
 when isMainModule:
   import std/os
+  import std/osproc
 
   if paramCount() == 1:
     emsdkEnv("emcc -v", paramStr(1))
   else:
     emsdkEnv("emcc -v")
+  discard execCmd("ls ~/.emsdkenv/emsdk_*/emsdk_env.sh | sed \"s|^$HOME|~|\" | sed 's/^/source /'")
