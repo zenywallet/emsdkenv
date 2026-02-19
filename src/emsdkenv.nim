9434a2499ec3034a71202ff42e254bbcb0563936
zenywallet <zenywallet@bitzeny.jp>
2026-02-20 00:40:42 +0900

    change emsdkenv command line

--- src/emsdkenv.nim
+++ src/emsdkenv.nim
@@ -7,9 +7,16 @@ export emsdkenv
 when isMainModule:
   import std/os
   import std/osproc
+  import std/posix
 
-  if paramCount() == 1:
-    emsdkEnv("emcc -v", paramStr(1))
+  let ver: string = if paramCount() == 1: paramStr(1) else: "latest"
+  let (ret, emsdkVer) = installEmsdk(ver)
+  if ret:
+    var shell = getEnv("SHELL")
+    if shell.len == 0:
+      shell = "/bin/sh"
+    var command = ". ~/.emsdkenv/emsdk_" & emsdkVer & "/emsdk_env.sh && emcc -v && " & shell
+    discard execl(shell, "sh", "-c", command, nil)
   else:
-    emsdkEnv("emcc -v")
-  discard execCmd("ls ~/.emsdkenv/emsdk_*/emsdk_env.sh | sed \"s|^$HOME|~|\" | sed 's/^/source /'")
+    echo "error: emsdk version [", ver, "] is not found"
+    discard execCmd("ls ~/.emsdkenv/emsdk_*/emsdk_env.sh | sed \"s|^$HOME|~|\" | sed 's/^/source /'")
